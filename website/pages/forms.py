5c41e8af1bd3d80cc4fbf45dc30a88cc2dc1814a
MattBxrclay <mob1crt@bolton.ac.uk>
2024-12-11 01:48:20 +0000

    Bootstrap
    Bootstrap was installed and implemented on registration page and basic navbar created and implemented on all pages

--- website/pages/forms.py
+++ website/pages/forms.py
@@ -9,6 +9,13 @@ class UserForm(ModelForm):
         model = User
         fields = ['id','name', 'email', 'password', 'role']
 
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control w-50'
+        })
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password'])  # Hash the password
